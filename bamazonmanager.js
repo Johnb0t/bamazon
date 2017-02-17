cc7c427516d97444f93aeeb41c12f45423c15e70
John Krug <john@Johns-MacBook-Pro.local>
2017-02-17 15:40:03 -0500

    added coloring and cleaned up code

--- bamazonmanager.js
+++ bamazonmanager.js
@@ -1,7 +1,6 @@
-//require packages
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var colors = require('colors');
+var colors = require('colors')
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -12,184 +11,179 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+function displayMenu() {
+	console.log("Menu Options:".bold.blue);
+	console.log("1.".red + "View Products for Sale".bold);
+	console.log("2.".red + "View Low Inventory".bold);
+	console.log("3.".red + "Add to Inventory".bold);
+	console.log("4.".red + "Add New Product".bold);
 
-//Create function to prompt user with menu options
-function startMenu() {
-	console.log("Menu Options".bold);
-	console.log("1. View Products for Sale");
-	console.log("2. View Low Inventory");
-	console.log("3. Add to Inventory");
-	console.log("4. Add New Product");
 	inquirer.prompt([{
 		type: "input",
-	  	name: "option",
-	  	message: "Choose a Menu Option:",
-	  	validate: function(value) {
-      	if (isNaN(value) === false && 0 > value < 4) {
-        return true;
-      	}
-      	return false;
-  	  	}
-	  }]).then(function(data) {
-	  	console.log("You chose option: ".bold + data.option);
-	  		
-	  		if(data.option == 1) {
-	  			connection.query("SELECT id, product_name, price, stock_quantity FROM products", function (error, results) {
-					if (error) {
-						return console.log("Error Accessing Data");
-					} else {
-						for (var i = 0; i < results.length; i++) {
-							console.log("ID: ".bold + results[i].id, "PRODUCT: ".bold + results[i].product_name, "PRICE: ".bold + results[i].price, "STOCK QUANTITY: ".bold + results[i].stock_quantity);
-							}
-							restart();
+		name: "option",
+		message: "Choose a menu option: ",
+		validate: function(value) {
+			if (isNaN(value) === false && 0 > value < 4) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}]).then(function(data) {
+		console.log("You chose option: " + data.option);
+
+		if (data.option == 1) {
+			connection.query("SELECT * FROM products", function (error, results) {
+				if (error) {
+					console.log("Error selecting table.");
+				} else {
+					for (var i = 0; i < results.length; i++) {
+						console.log("ID: ".bold + results[i].id + " Product: ".bold + results[i].product_name + 
+							" Stock Quantity: ".bold + results[i].stock_quantity);
 					}
-				}) 
+					restart();
+				}
+			})
+		} else if (data.option == 2) {
+			connection.query("SELECT product_name, stock_quantity FROM products WHERE stock_quantity < 5", function (error, results) {
+				if (error) {
+					console.log("Error accessing data");
+				} else {
+					for (var i = 0; i < results.length; i++) {
+						console.log("Product: ".bold.red + results[i].product_name + " Stock Quantity: ".bold.red + results[i].stock_quantity);
+					}
+					restart();
+				}
+			})
+		} else if (data.option == 3) {
+			connection.query("SELECT * FROM products", function (error, results) {
+				if (error) {
+					console.log("Error accessing data");
+				} else {
+					for (var i = 0; i < results.length; i++) {
+					console.log("ID: ".bold + results[i].id + " Product: ".bold + results[i].product_name + 
+						" Price: ".bold + results[i].price + " Stock Quantity: ".bold + results[i].stock_quantity);
+					};
 
-	  		} else if (data.option == 2) {
-	  			connection.query("SELECT product_name, stock_quantity FROM products WHERE stock_quantity < 5", function (error, results) {
-	  				for (var i = 0; i < results.length; i++) {
-	  					console.log("PRODUCT: ".bold + results[i].product_name, "STOCK QUANTITY: ".bold + results[i].stock_quantity);
-	  				}
-	  				restart();
-	  			}) 
-	  		} else if(data.option == 3) {
-	  			connection.query("SELECT id, product_name, price, stock_quantity FROM products", function (error, results) {
-					if (error) {
-						return console.log("Error Accessing Data");
-					} else {
-						for (var i = 0; i < results.length; i++) {
-							console.log("ID: ".bold + results[i].id, "PRODUCT: ".bold + results[i].product_name, "PRICE: ".bold + results[i].price, "STOCK QUANTITY: ".bold + results[i].stock_quantity);
+					inquirer.prompt([{
+						type: "input",
+						name: "prod_id",
+						message: "Which product do you want to update: ",
+						validate: function(value) {
+							if (isNaN(value) == false && value > 0 && value < results.length + 1) {
+								return true;
+							} else {
+								return false;
 							}
-							inquirer.prompt([{
-								type: "input",
-							  	name: "product_id",
-							  	message: "Which product would you like to update?",
-							  	validate: function(value) {
-						      	if (isNaN(value) === false && value > 0 && value < results.length + 1) {
-						        return true;
-						      	}
-						      	return false;
-						  	  	}
-						  	  
-								}, {
-							 
-								type: "input",
-								name: "update_quant",
-								message: "How much would you like to add?",
-								validate: function(value) {
-						      	if (isNaN(value) === false && value > 0) {
-						        return true;
-						      	}
-						      	return false;
-						  	  	}
-						  		}]).then(function(data) {
-						  			var id = data.product_id;
-						  			var quant = data.update_quant;
+						}
+					}, {
+						type: "input",
+						name: "update_quant",
+						message: "Quantity to add: ",
+						validate: function(value) {
+							if (isNaN(value) == false && value > 0) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					}]).then(function(data) {
+						var id = data.prod_id;
+						var quant = data.update_quant;
 
-						  			connection.query("UPDATE products SET stock_quantity = (stock_quantity + " + quant + ") WHERE id = " + id, function (error, results) {
-										if (error) {
-											return console.log("There was an error updating the products table.");
-												}
+						connection.query("UPDATE products SET stock_quantity = stock_quantity + " + quant + 
+							" WHERE id = " + id, function (error, results) {
+								if (error) {
+									console.log("An error occured while updating products table");
+								};
+								restart();
+							})
+					})
+				}
+			})
+		} else if (data.option == 4) {
+			console.log("Departments: ");
+			connection.query("SELECT * FROM departments", function (error, results) {
+				if (error) {
+					console.log("Error accessing departments table");
+				} else {
+					for (var i = 0; i < results.length; i++) {
+						console.log("ID: " + results[i].id + " Department Name: " + results[i].department_name);
+					};
+				
+					inquirer.prompt([{
+						type: "input",
+						name: "dept_id",
+						message: "Which department would you like to update: ",
+						validate: function(value) {
+							if (isNaN(value) == false && value > 0) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					}, {
+						type: "input",
+						name: "prod_name",
+						message: "Name of product: "
+					}, {
+						type: "input",
+						name: "prod_price",
+						message: "Price of product: ",
+						validate: function(value) {
+							if (isNaN(value) == false && value > 0) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					}, {
+						type: "input",
+						name: "prod_quant",
+						message: "Quantity to add: ",
+						validate: function(value) {
+							if (isNaN(value) == false && value > 0) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					}]).then(function(data) {
+						var dept = data.dept_id;
+						var item = data.prod_name;
+						var price = data.prod_price;
+						var quant = data.prod_quant;
 
-												restart();
-										});
-						  		})
-						  	}
-						  }) 
-	  		}
-
-							 else if (data.option == 4) {
-								console.log("AVAILABLE DEPARTMENTS:".bold);
-								connection.query("SELECT * FROM departments", function (error, results) {
-					  				for (var i = 0; i < results.length; i++) {
-					  					console.log("ID: ".bold + results[i].id, "Department Name: ".bold + results[i].department_name);
-					  				}
-					  			
-								inquirer.prompt([{
-									type: "input",
-									name: "department_id",
-									message: "Which department would you like to add to?",
-									validate: function(value) {
-							      	if (isNaN(value) === false && value > 0) {
-							        return true;
-							      	}
-							      	return false;
-							        }
-
-							  	  	}, {
-
-									type: "input",
-								  	name: "product_name",
-								  	message: "What is the name of the product?",
-								  								  	  
-									}, {
-								 
-									type: "input",
-									name: "price",
-									message: "How much does it cost?",
-									validate: function(value) {
-							      	if (isNaN(value) === false && value > 0) {
-							        return true;
-							      	}
-							      	return false;
-							        }
-
-							  	  	}, {
-
-							  	  	type: "input",
-									name: "update_quant",
-									message: "How much would you like to add?",
-									validate: function(value) {
-							      	if (isNaN(value) === false && value > 0) {
-							        return true;
-							      	}
-							      	return false;
-							  	  	}
-							  	  	}]).then(function(data) {
-							  	  		var item = data.product_name;
-							  	  		var price = data.price;
-						  				var quant = data.update_quant;
-						  				var dept = data.department_id;
-
-						  				connection.query("INSERT INTO products (product_name, department_id, price, stock_quantity) VALUES('" + item + "', " + dept + ", " + price + ", " + quant + ")");
-						  				console.log("ITEM ADDED".bold)
-						  				connection.query("SELECT * FROM products", function (error, results) {
-							  				for (var i = 0; i < results.length; i++) {
-							  					console.log("ID: ".bold + results[i].id, "PRODUCT NAME: ".bold + results[i].product_name, "DEPARTMENT ID: ".bold + results[i].department_id, "PRICE: ".bold + results[i].price, "STOCK QUANTITY: " + results[i].stock_quantity);
-							  				}
-							  				restart();
-							  			});
-							  	  });
-							}); 
+						connection.query("INSERT INTO products (product_name, department_id, price, stock_quantity)" + 
+							" VALUE ('" + item + "', " + dept + ", " + price + ", " + quant + ")");
+						console.log("\nITEM ADDED\n");
+						connection.query("SELECT * FROM products", function (error, results) {
+							for (var i = 0; i < results.length; i++) {
+								console.log("ID: ".bold + results[i].id + " Product: ".bold + results[i].product_name + 
+									" Department: ".bold + results[i].department_id + " Price: ".bold + results[i].price + 
+									" Quantity: ".bold + results[i].stock_quantity);
+							};
+							restart();
+						});
+					});
 				};
+			});
+		};
+	});
+};
 
-	  		
-
-	  });
-
-
-}
-startMenu();
-
-
-
-
-///functions
+displayMenu();
 
 function restart() {
 	inquirer.prompt([{
 		type: "confirm",
-	  	name: "restart",
-	  	message: "Do you want to continue Supervisor View?".bold,
-	  	}]).then(function(data) {
-	  		if(!data.restart){
-	  			connection.end();
-	  		} else {
-	  			startMenu();
-	  		};
-	  	});
+		name: "restart",
+		message: "Return to menu? (Will exit otherwise)"
+	}]).then(function(data) {
+		if (!data.restart) {
+			connection.end();
+		} else {
+			displayMenu();
+		};
+	});
 };
-
-
-//Do something with user choice.
-
